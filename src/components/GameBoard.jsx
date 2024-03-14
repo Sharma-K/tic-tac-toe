39ef1b63fe94efddea350479a3f6830f2f7d2c8e
Keshav Sharma <keshav2743@gmail.com>
2024-03-14 23:38:21 +0530

    added gameBoard

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,7 +1,32 @@
+import { useState } from "react";
+const initialGameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+];
+
 export default function GameBoard(){
+    const [gameBoard, setGameBoard] = useState(initialGameBoard); 
+    function handleSelectSquare(row, col){
+        setGameBoard((prevGameBoard) => {
+            const updatedBoard = [...prevGameBoard.map(inner => [...inner])];
+            updatedBoard[row][col] = 'X';
+            return updatedBoard;
+        })
+    }
     return (
         <ol id="game-board">
-            
+        {gameBoard.map((row, index) => (
+            <li key={index}>
+                <ol>
+                    {row.map((col, cindex) => (
+                        <li key={cindex}>
+                            <button onClick={() => handleSelectSquare(index, cindex)}>{col}</button>
+                        </li>
+                    ))}
+                </ol>
+            </li>
+        ))}
         </ol>
     )
 }
